103aeeebba977385bca00b2391e22ebb892e7b16
WellitonBecker <welliton.lbecker@gmail.com>
2026-01-18 13:30:46 -0300

    feat: Implement structural changes to enhance code organization and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark min-h-screen bg-background text-foreground`}
       >
+        <nav className="sticky top-0 z-40 w-full flex items-center justify-end py-2 px-4 dark:bg-[#0f1816]/95 backdrop-blur supports-backdrop-filter:bg-[background]/60 border-b border-border gap-2">
+          <div className="flex-1">
+            <Button asChild variant={"outline"}>
+              <Link href={`/`}>Home</Link>
+            </Button>
+          </div>
+          <Button className="ml-4" asChild>
+            <Link href={`/1/dashboard`}>
+              {/* <LayoutDashboard /> */}
+              Meu Dashboard
+            </Link>
+          </Button>
+          {/* <ModeToggle /> */}
+        </nav>
         {children}
+        <footer>
+          <div className="flex items-center justify-center p-4 bg-black text-white font-sans">
+            <span className="text-sm text-zinc-500 flex flex-col sm:flex-row gap-2 text-center">
+              <span>
+                © 2026 Craque do Pretinho Básico. All rights reserved.{" "}
+              </span>
+              <span>Desenvolvido por Welliton Luiz Becker.</span>
+            </span>
+          </div>
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,40 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Calendar, Clock, Trophy } from "lucide-react";
+import {
+  Calendar,
+  CalendarIcon,
+  Clock,
+  Crown,
+  Frown,
+  Heart,
+  Trophy,
+} from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black font-sans text-white ">
-      <main className="flex min-h-screen w-full max-w-6xl flex-col px-4 ">
+    <div className="flex min-h-screen items-center justify-center bg-black font-sans text-white py-4 ">
+      <main className="flex min-h-screen w-full max-w-4xl min-w-xs flex-col px-4 gap-4 ">
         <div className="h-16 ">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <p className="text-zinc-300 font-light">
             Estatísticas do Pretinho Básico
           </p>
         </div>
-        <Card className="gap-2 bg-[#132e20] bg-linear-to-tr from-[#132e20] via-[#060a09] to-[#132e20] border border-green-800/30 shadow-2xl shadow-primary/20">
+        <Card className="gap-2 bg-[#132e20] bg-linear-to-tr from-[#132e20] via-[#0f1816] to-[#132e20] border border-[#14201d]">
           <CardHeader>
             <CardTitle className="text-start text-sm uppercase font-semibold text-primary">
               <Trophy className="inline-block mr-2 h-6 w-6" />
               Craque da última edição
             </CardTitle>
           </CardHeader>
-          <CardContent className="flex flex-row gap-6 items-center">
+          <CardContent className="flex flex-col sm:flex-row gap-6 items-center">
             <Avatar className="size-40 border-2 border-primary/50">
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarImage src="/pora.png" alt="Porã Bernardes" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <div className="flex flex-col items-start justify-between h-full gap-4 text-muted/80">
-              <h1 className="text-4xl font-bold text-primary">Potter</h1>
+            <div className="flex flex-col items-center sm:items-start justify-between h-full gap-4 text-muted/80">
+              <h1 className="text-4xl font-bold text-primary">Porã</h1>
               <div className="flex gap-4">
                 <div className="flex items-center gap-1.5">
                   <Calendar className="h-4 w-4" />
@@ -41,6 +49,106 @@ export default function Home() {
                 8<span className="text-sm text-muted/80">votos recebidos</span>
               </Badge>
             </div>
+          </CardContent>
+        </Card>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <Card className="gap-2 bg-[#132e20] bg-linear-to-tr from-[#132e20] via-[#0f1816] to-[#132e20] border-[#14201d]">
+            <CardHeader>
+              <CardTitle className="text-start text-sm w-full flex justify-between uppercase font-semibold text-muted/70">
+                Maior Vencedor
+                <Crown className="inline-block mr-2 h-4 w-4 text-primary" />
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-row gap-2 items-center">
+              <Avatar className="size-14 border-2 border-primary/50">
+                <AvatarImage src="/pora.png" alt="Porã Bernardes" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col items-start justify-center h-full">
+                <h1 className="text-xl font-bold text-muted">Porã</h1>
+                <div className="flex text-sm gap-1">
+                  <span className="text-primary">8</span>
+                  <span className="text-muted/70">títulos</span>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="gap-2 bg-[#4a3317] bg-linear-to-tr from-[#4a3317] via-[#0f1816] to-[#4a3317] border-[#14201d]">
+            <CardHeader>
+              <CardTitle className="text-start text-sm w-full flex justify-between uppercase font-semibold text-muted/70">
+                Maior Puxa saco
+                <Heart className="inline-block mr-2 h-4 w-4 text-amber-500" />
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-row gap-2 items-center">
+              <Avatar className="size-14 border-2 border-amber-500/50">
+                <AvatarImage src="/pora.png" alt="Porã Bernardes" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col items-start justify-center h-full">
+                <h1 className="text-xl font-bold text-muted">Porã</h1>
+                <div className="flex text-sm gap-1">
+                  <span className="text-amber-400">4x → Fetter</span>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="gap-2 bg-[#4a1a17] bg-linear-to-tr from-[#4a1a17] via-[#0f1816] to-[#4a1a17] border-[#14201d]">
+            <CardHeader>
+              <CardTitle className="text-start text-sm w-full flex justify-between uppercase font-semibold text-muted/70">
+                O Injustiçado
+                <Frown className="inline-block mr-2 h-4 w-4 text-red-500" />
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-row gap-2 items-center">
+              <Avatar className="size-14 border-2 border-red-500/50">
+                <AvatarImage src="/pora.png" alt="Porã Bernardes" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col items-start justify-center h-full">
+                <h1 className="text-xl font-bold text-muted">Porã</h1>
+                <div className="flex text-sm gap-1">
+                  <span className="text-red-500">8</span>
+                  <span className="text-muted/70">votos sem título</span>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className="gap-2 bg-[#0f1816] border-[#14201d]">
+          <CardHeader>
+            <CardTitle className="text-start text-sm w-full uppercase font-semibold text-muted/70">
+              <CalendarIcon className="inline-block mr-2 h-6 w-6" />
+              Últimas Edições
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center">
+            {[1, 2, 3, 4, 5].map((edition, i) => (
+              <div
+                key={i}
+                className="flex w-full gap-4 hover:bg-[#14201d] py-2 px-4 rounded-lg cursor-pointer not-first:border-t not-first:border-[#14201d]"
+              >
+                <Avatar className="size-12">
+                  <AvatarImage src="/pora.png" alt="Porã Bernardes" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <div className="flex flex-1 flex-col items-start justify-between text-muted/80 ">
+                  <h1 className="text-lg font-medium text-muted">Porã</h1>
+                  <div className="flex gap-4">
+                    <div className="flex items-center gap-1.5">
+                      <Calendar className="h-4 w-4" />
+                      <span className="text-xs font-extralight">
+                        16/01/2026
+                      </span>
+                    </div>
+                    <div className="flex items-center gap-1.5">
+                      <Clock className="h-4 w-4" />
+                      <span className="text-xs font-extralight">13h</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </CardContent>
         </Card>
       </main>
